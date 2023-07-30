403751794b42655cfdcfeb7ff55be187402a7d20
SATRIO WIBISONO <satrio_wibisono@students.amikom.ac.id>
2023-07-30 20:27:31 +0700

    update index

--- API/index.js
+++ API/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../CLIENT/public/upload");
+    cb(null, "../CLIENT_BARU/public/upload");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
